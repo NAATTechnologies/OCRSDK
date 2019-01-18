12f594110a98aeb4a8c6860ebaba68225b7bf26a
Roberto Carrera <rcarrera@na-at.com.mx>
2019-01-18 12:17:16 -0600

    Versión inicial

---
+++ OCRSDK.framework/Headers/OCRSDK.h
@@ -0,0 +1,21 @@
+//
+//  FADOCR.h
+//  FADOCR
+//
+//  Created by Roberto Carrera on 12/11/18.
+//  Copyright © 2018 MuukTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#include <CommonCrypto/CommonCrypto.h>
+
+//! Project version number for FADOCR.
+FOUNDATION_EXPORT double OCRSDKVersionNumber;
+
+//! Project version string for FADOCR.
+FOUNDATION_EXPORT const unsigned char OCRSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FADOCR/PublicHeader.h>
+
+
